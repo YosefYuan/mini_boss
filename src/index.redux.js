100fa7dc01c14d3b0dc24931d81f36d53d955629
yosef yuan <yuanruixiangjet@163.com>
2018-11-05 16:59:08 +0800

    4-3 link to redux by self

---
+++ src/index.redux.js
@@ -0,0 +1,22 @@
+const ADD = 'ADD'
+const MINUS = 'MINUS'
+
+// reducer
+export function counter(state = 0, action) {
+    switch (action.type) {
+        case 'ADD':
+            return state + 1;
+        case 'MINUS':
+            return state - 1;
+        default:
+            return 10
+    }
+}
+
+export function addGun(){
+    return {type: ADD}
+}
+
+export function minusGun(){
+    return {type: MINUS}
+}
