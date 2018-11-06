ef85f80113443e311dca8e66f358f6a41233cfd6
yosef yuan <yuanruixiangjet@163.com>
2018-11-06 13:38:42 +0800

    decorators config ok

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {addGun,minusGun,addGunAsync} from './index.redux'
 
+const mapStateToProps = (state) => ({
+    num: state
+})
+const actionCreator = {addGun,minusGun,addGunAsync}
+// App = connect(mapStateToProps,actionCreator)(App)
+@connect(
+    (state) => ({
+        num: state
+    }),
+    {addGun,minusGun,addGunAsync}
+)
 class App extends React.Component {
     render() {
-        const store = this.props.store
-        const num = store.getState()
-        const addGun = this.props.addGun
-        const minusGun = this.props.minusGun
-        const addGunAsync = this.props.addGunAsync
         return (
             <div>
-                <h1>现在有机枪{num}把</h1>
-                <button onClick={() => store.dispatch(addGun())}>ADD</button>
-                <button onClick={() => store.dispatch(minusGun())}>MINUS</button>
-                <button onClick={() => store.dispatch(addGunAsync())}>MINUS in 2 seconds</button>
-            </div>)
+                <h1>现在有机枪{this.props.num}把</h1>
+                <button onClick={this.props.addGun}>ADD</button>
+                <button onClick={this.props.minusGun}>MINUS</button>
+                <button onClick={this.props.addGunAsync}>MINUS in 2 seconds</button>
+            </div>
+        )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import App from "./App"
-import { counter, addGun, minusGun,addGunAsync } from './index.redux'
+import { counter, addGun, minusGun, addGunAsync } from './index.redux'
 
 const store = createStore(counter, applyMiddleware(thunk))
+console.log(store.getState())
 
-function render() {
-    ReactDom.render(<App store={store} addGun={addGun} minusGun={minusGun} addGunAsync={addGunAsync} />, document.getElementById('root'))
-}
-
-render()
-
-store.subscribe(render)
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
