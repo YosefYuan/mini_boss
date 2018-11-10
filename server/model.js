992a1437c261a82d905a7f51152f1797070bb70f
yosef yuan <yuanruixiangjet@163.com>
2018-11-10 11:34:59 +0800

    6-9 登录注册-注册前后端联调

--- server/model.js
+++ server/model.js
@@ -1,3 +1,31 @@
 const mongoose = require('mongoose')
 const DB_URL = 'mongodb://127.0.0.1:27017/mini_boss'
 mongoose.connect(DB_URL)
+
+const models = {
+    user: {
+        'user': { 'type': String, 'require': true },
+        'pwd': { 'type': String, 'require': true },
+        'type': { 'type': String, 'require': true },
+        // 头像
+        'avatar': { 'type': String },
+        // 个人简介
+        'desc': { 'type': String },
+        // 职位名称
+        'avatar': { 'type': String },
+        // boss特有字段
+        'company': { 'type': String },
+        'money': { 'type': String },
+    },
+    chat: {}
+}
+
+for (let m in models) {
+    mongoose.model(m, new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+    getModule: function (name) {
+        return mongoose.model(name)
+    }
+}
