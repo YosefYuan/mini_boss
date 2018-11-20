9ef9b5d6bf9884bd0c8e72154f3725b80bf84caa
Yosef Yuan <yuanruixiangjet@163.com>
2018-11-20 10:51:18 +0800

    test in office piaoniu

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
-import { Provider } from 'react-redux'
+import React from "react"
+import ReactDom from "react-dom"
+import { createStore, applyMiddleware, compose } from "redux"
+import { Provider } from "react-redux"
 import {
-    BrowserRouter,
-    Route,
-    Switch
-} from 'react-router-dom'
-import thunk from 'redux-thunk'
-import reducers from './reducer'
-import Login from './container/login/login'
-import BossInfo from './container/bossinfo/bossinfo'
-import GeniusInfo from './container/geniusinfo/geniusinfo'
-import Register from './container/register/register'
-import AuthRoute from './component/authRoute/authRoute'
-import Dashboard from './component/dashboard/dashboard'
-import './index.css'
+	BrowserRouter,
+	Route,
+	Switch
+} from "react-router-dom"
+import thunk from "redux-thunk"
+import reducers from "./reducer"
+import Login from "./container/login/login"
+import BossInfo from "./container/bossinfo/bossinfo"
+import GeniusInfo from "./container/geniusinfo/geniusinfo"
+import Register from "./container/register/register"
+import AuthRoute from "./component/authRoute/authRoute"
+import Dashboard from "./component/dashboard/dashboard"
+import "./index.css"
 
 const store = createStore(
-    reducers,
-    compose(
-        applyMiddleware(thunk),
-        window.devToolsExtension ? window.devToolsExtension() : f => f
-    )
+	reducers,
+	compose(
+		applyMiddleware(thunk),
+		window.devToolsExtension ? window.devToolsExtension() : f => f
+	)
 )
 
 ReactDom.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <div>
-                <AuthRoute></AuthRoute>
-                <Switch>
-                    <Route path='/bossinfo' component={BossInfo}></Route>
-                    <Route path='/geniusinfo' component={GeniusInfo}></Route>
-                    <Route path='/login' component={Login}></Route>
-                    <Route path='/register' component={Register}></Route>
-                    <Route component={Dashboard}></Route>
-                </Switch>
-            </div>
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
+	<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				<AuthRoute></AuthRoute>
+				<Switch>
+					<Route path='/bossinfo' component={BossInfo}></Route>
+					<Route path='/geniusinfo' component={GeniusInfo}></Route>
+					<Route path='/login' component={Login}></Route>
+					<Route path='/register' component={Register}></Route>
+					<Route component={Dashboard}></Route>
+				</Switch>
+			</div>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById("root")
 )
